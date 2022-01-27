f6b0887b6510709dab448cf4d2084169357161e4
Brauliorg23 <up180167@alumnos.upa.edu.mx>
2022-01-27 12:27:45 -0600

    Creacion del inicio del server y del cliente, ademas de la instalacion de paquetes

--- nuevo inicio/client/src/App.js
+++ nuevo inicio/client/src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.scss';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 function App() {
+  
   return (
+  <Router>
     <div className="app">
-      <h1>hola hhh</h1>
+      <h1>Sistema de rutas basico</h1>
+      
+      <Route>holas</Route>
+      {/* <Route path="/" exact component={Home}/> */}
     </div>
+  </Router>
   );
+}
+
+function Home (){
+  return <h2>Estamos en el inicio</h2>
+}
+function Contact(){
+  return <h2>Componente contacto</h2>
+}
+function Users(){
+  return <h2>Estamos en el componente users</h2>
 }
 
 export default App;
